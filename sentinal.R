88e597674516505d06bf770780a7883bca5d29c2
siddhinath taldhi <sid020898@gmail.com>
2019-02-15 23:25:41 +0530

    fixed object not declared

--- sentinal.R
+++ sentinal.R
@@ -21,15 +21,15 @@ access_secret ="provide your details here" #access secret from twitter app
 # set up
 setup_twitter_oauth(consumer_key,consumer_secret,access_token, access_secret)
 
-#extracting tweets of a particular hashtag, "n" is the no of tweets to be extracted.
+# extracting tweets of a particular hashtag, "n" is the no of tweets to be extracted.
 tweets_g <- searchTwitter("#google", n=500,lang = "en")
 
-#Convert this extracted data to a dataframe which makes it more readable and easier to work with.
+# Convert this extracted data to a dataframe which makes it more readable and easier to work with.
 google_tweets <- twListToDF(tweets_g)
 View(google_tweets)
 google_text<- google_tweets$text
 
-#converting text to lower case
+# converting text to lower case
 google_text<- tolower(google_text) 
 
 # Replace blank space (“rt”)
@@ -53,23 +53,23 @@ google_text <- gsub("^ ", "", google_text)
 # Remove blank spaces at the end
 google_text <- gsub(" $", "", google_text)
 
-#clean up by removing stop words
+# clean up by removing stop words
 google_tweets.text.corpus <- tm_map(google_tweets.text.corpus, function(x)removeWords(x,stopwords()))
 
-#generate wordcloud
+# generate wordcloud
 wordcloud(google_tweets.text.corpus,min.freq = 10,colors=brewer.pal(8, "Dark2"),random.color = TRUE,max.words = 500)
 
-#getting emotions using in-built function
+# getting emotions using in-built function
 mysentiment_google<-get_nrc_sentiment((google_text))
 
-#calculationg total score for each sentiment
+# calculationg total score for each sentiment
 Sentimentscores_google<-data.frame(colSums(mysentiment_google[,]))
 
 names(Sentimentscores_google)<-"Score"
 Sentimentscores_google<-cbind("sentiment"=rownames(Sentimentscores_google),Sentimentscores_google)
 rownames(Sentimentscores_google)<-NULL
 
-#plotting the sentiments with scores
+# plotting the sentiments with scores
 ggplot(data=Sentimentscores_google,aes(x=sentiment,y=Score))+geom_bar(aes(fill=sentiment),stat = "identity")+
   theme(legend.position="none")+
   xlab("Sentiments")+ylab("scores")+ggtitle("Sentiments of people behind the tweets on tech giant GOOGLE")
